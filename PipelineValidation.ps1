1b9ffe1f72dbb9877ce04ab9a2eee6340811865d
Mikhail <rodionovmv@gmail.com>
2021-11-17 16:12:39 +0100

    Update PipelineValidation.ps1

--- PipelineValidation.ps1
+++ PipelineValidation.ps1
@@ -1,4 +1,4 @@
-# Install required  Module
+# Install required module
 Install-Module -Name VSTeam -Scope CurrentUser
 # Specify your Azure DevOps project
 Set-VSTeamAccount -Account <Name of your Azure DevOps org> -PersonalAccessToken <Your PAT with access to your project>
