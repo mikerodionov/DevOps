231ff1dc3b0c1b9b08623f0c218b29d2d10b89ed
Mikhail <rodionovmv@gmail.com>
2021-11-17 16:11:49 +0100

    Create PipelineValidation.ps1
    Local pipeline template validation

---
+++ PipelineValidation.ps1
@@ -0,0 +1,6 @@
+# Install required  Module
+Install-Module -Name VSTeam -Scope CurrentUser
+# Specify your Azure DevOps project
+Set-VSTeamAccount -Account <Name of your Azure DevOps org> -PersonalAccessToken <Your PAT with access to your project>
+# Request pipeline validation:
+Test-VSTeamYamlPipeline -Project <PROJECT_NAME> -PipelineId <PIPELINE_ID> -FilePath '<PATH_TO_YAML_FILE>'
