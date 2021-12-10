414d187d74328b423ac11c29acb9ccab37552a94
Mikhail Rodionov <rodionovmv@gmail.com>
2021-12-10 17:12:20 +0100

    Update WorkingWithGit.sh

--- WorkingWithGit.sh
+++ WorkingWithGit.sh
@@ -1,4 +1,4 @@
 # Wnen remote branch was deleted on GitHub/in Azure DevOps repo you will still need to delete your local branch
 git branch -d local_branch_name
-# To merge between main and working branch, pull main branch then switch to working and run
+# To merge between main and working branch, pull main branch then switch to working and run (when working gets behing main)
 git merge main
