0f9c6c5021dc2672eecd7a7900098b81719cdcf1
Mikhail Rodionov <rodionovmv@gmail.com>
2022-01-28 10:43:33 +0100

    Update privateLinkServiceNetworkPolicies.ps1

--- PowerShell/privateLinkServiceNetworkPolicies.ps1
+++ PowerShell/privateLinkServiceNetworkPolicies.ps1
@@ -3,18 +3,14 @@
 # Script below checks whether privateLinkServiceNetworkPolicies enabled for subnet, when enabled it prevents you from creating PEs within the subnet
 # MSFT docs: https://docs.microsoft.com/en-us/azure/private-link/disable-private-endpoint-network-policy
 $virtualSubnetName = "default"
-$virtualNetwork= Get-AzVirtualNetwork `
-  -Name "myVirtualNetwork" ` 
-  -ResourceGroupName "myResourceGroup"
+$virtualNetwork= Get-AzVirtualNetwork -Name "myVirtualNetwork" -ResourceGroupName "myResourceGroup"
 ($virtualNetwork | Select -ExpandProperty subnets | Where-Object  {$_.Name -eq $virtualSubnetName}).privateLinkServiceNetworkPolicies
 
 # Disable PrivateEndpointNetworkPolicies
 # Make sure to connect with Connect-AzAccount
 # Make sure to switch to the right subscription with Set-AzContext
-$net =@{
-    Name = 'myVNet'
-    ResourceGroupName = 'myResourceGroup'
-}
-$vnet = Get-AzVirtualNetwork @net
-($vnet | Select -ExpandProperty subnets | Where-Object {$_.Name -eq 'default'}).PrivateEndpointNetworkPolicies = "Disabled"
-$vnet | Set-AzVirtualNetwork
+$virtualSubnetName = "default"
+$virtualNetwork= Get-AzVirtualNetwork -Name "myVirtualNetwork" -ResourceGroupName "myResourceGroup"
+$virtualNetwork = Get-AzVirtualNetwork @net
+($virtualNetwork | Select -ExpandProperty subnets | Where-Object {$_.Name -eq $virtualSubnetName}).PrivateEndpointNetworkPolicies = "Disabled"
+$virtualNetwork | Set-AzVirtualNetwork
