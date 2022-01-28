bb2703f5c3654a7f6076b9b133fcf42d867cf8e5
Mikhail Rodionov <rodionovmv@gmail.com>
2022-01-28 10:28:24 +0100

    Update privateLinkServiceNetworkPolicies.ps1

--- PowerShell/privateLinkServiceNetworkPolicies.ps1
+++ PowerShell/privateLinkServiceNetworkPolicies.ps1
@@ -7,3 +7,14 @@ $virtualNetwork= Get-AzVirtualNetwork `
   -Name "myVirtualNetwork" ` 
   -ResourceGroupName "myResourceGroup"
 ($virtualNetwork | Select -ExpandProperty subnets | Where-Object  {$_.Name -eq $virtualSubnetName}).privateLinkServiceNetworkPolicies
+
+# Disable PrivateEndpointNetworkPolicies
+# Make sure to connect with Connect-AzAccount
+# Make sure to switch to the right subscription with Set-AzContext
+$net =@{
+    Name = 'myVNet'
+    ResourceGroupName = 'myResourceGroup'
+}
+$vnet = Get-AzVirtualNetwork @net
+($vnet | Select -ExpandProperty subnets | Where-Object {$_.Name -eq 'default'}).PrivateEndpointNetworkPolicies = "Disabled"
+$vnet | Set-AzVirtualNetwork
