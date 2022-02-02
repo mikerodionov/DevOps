9e59d6aed516ec27acb500ab1bcac7d50a47f9ee
Mikhail Rodionov <rodionovmv@gmail.com>
2022-02-02 15:59:42 +0100

    Update WorkingWithGit.sh

--- Git/WorkingWithGit.sh
+++ Git/WorkingWithGit.sh
@@ -1,4 +1,12 @@
+# To delete remote branch
+git push RemoteName --delete BranchName
 # Wnen remote branch was deleted on GitHub/in Azure DevOps repo you will still need to delete your local branch
 git branch -d local_branch_name
+# Once remote branch was deleted you need to propogate changes locally
+# fectch - to download objects and refs from another repository/repositories
+# --all - specifies to fetch all remotes
+# --prune - specifies to remove any remote-tracking references that no longer exist on the remote before fetching
+git fetch --all --prune
+
 # To merge between main and working branch, pull main branch then switch to working and run (when working gets behing main)
 git merge main
