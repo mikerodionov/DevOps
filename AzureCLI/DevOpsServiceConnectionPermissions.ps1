634853ecfe36cbbf264c10f576fe83f38771e628
Mikhail Rodionov <rodionovmv@gmail.com>
2022-02-02 17:08:53 +0100

    Update DevOpsServiceConnectionPermissions.ps1

--- AzureCLI/DevOpsServiceConnectionPermissions.ps1
+++ AzureCLI/DevOpsServiceConnectionPermissions.ps1
@@ -1,12 +1,19 @@
 $projectName = '%DevOps_Project_Name%'
 $groupname = '%DevOps_Project_Group%'
-$namespace = '%Namespace_GUID%' # Namespace GUID
+# https://docs.microsoft.com/en-us/azure/devops/organizations/security/namespace-reference?view=azure-devops
+$namespace = '%Namespace_GUID%' # Namespace GUID - to refer specific Azure DevOps resource
 
 $Project = az devops project list --query "value[?name=='$projectName'].{name:name,ID:id}"
 $projectId = ($Project | ConvertFrom-Json ).id
 $group = az devops security group list --scope project --project $projectName --query "graphGroups[?displayName=='$groupName']"
 $descriptor = ($group | ConvertFrom-Json).descriptor
 $token = 'endpoints/{0}' -f $projectId
+
+# View Project Permissions
+az devops security permission show `
+--id $projectId `
+--subject $descriptor `
+--token $token
 
 # Set Project Permissions
 az devops security permission update `
