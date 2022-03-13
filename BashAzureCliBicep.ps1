2b26017aac25ca7b2502e58510b1158cd24d20af
Mikhail Rodionov <rodionovmv@gmail.com>
2022-03-13 20:27:05 +0100

    Update and rename Bicep/AzureCliBicep.sh to BashAzureCliBicep.ps1

--- BashAzureCliBicep.ps1
+++ BashAzureCliBicep.ps1
@@ -1,11 +1,7 @@
 sudo apt install azure-cli
 # Check version
 az version
-# Upgrade in Windows Terminal, for Bash use packaage manager instead
-az upgrade
 az --help
-# Install Bicep CLI
-az bicep install
 az bicep --help
 az bicep version
 az bicep upgrade
