ebafba07e9a52b12a82b6c9ca7ebc2379f699766
Mikhail Rodionov <rodionovmv@gmail.com>
2022-03-13 20:23:27 +0100

    Update AzureCliBicep.sh

--- Bicep/AzureCliBicep.sh
+++ Bicep/AzureCliBicep.sh
@@ -1,7 +1,10 @@
 sudo apt install azure-cli
+# Check version
 az version
+# Upgrade in Windows Terminal, for Bash use packaage manager instead
 az upgrade
 az --help
+# Install Bicep CLI
 az bicep install
 az bicep --help
 az bicep version
