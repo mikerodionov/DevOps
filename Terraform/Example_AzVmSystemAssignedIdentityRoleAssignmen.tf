19b24c316a0c10f30b424efc6199c8f2e357f862
Mikhail Rodionov <rodionovmv@gmail.com>
2022-07-08 16:44:21 +0200

    Create Example_AzVmSystemAssignedIdentityRoleAssignmen.tf

---
+++ Terraform/Example_AzVmSystemAssignedIdentityRoleAssignmen.tf
@@ -0,0 +1,6 @@
+resource "azurerm_role_assignment" "your-vm-identity-blob-read-access" {
+  scope                = data.azurerm_storage_account.yourstorageaccount.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_virtual_machine.your-vm.identity[0].principal_id
+  skip_service_principal_aad_check = true
+}
