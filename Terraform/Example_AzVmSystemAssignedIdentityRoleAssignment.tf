bd55c94db3f55cf97c681a16e323d991cd1be6a1
Mikhail Rodionov <rodionovmv@gmail.com>
2022-07-08 16:45:42 +0200

    Update Example_AzVmSystemAssignedIdentityRoleAssignment.tf

--- Terraform/Example_AzVmSystemAssignedIdentityRoleAssignment.tf
+++ Terraform/Example_AzVmSystemAssignedIdentityRoleAssignment.tf
@@ -1,3 +1,8 @@
+data "azurerm_storage_account" "yourstorageaccount" {
+  name                = "yourstorageaccount"
+  resource_group_name = azurerm_resource_group.yourstorageaccount-rg.name
+}
+
 resource "azurerm_role_assignment" "your-vm-identity-blob-read-access" {
   scope                = data.azurerm_storage_account.yourstorageaccount.id
   role_definition_name = "Storage Blob Data Reader"
