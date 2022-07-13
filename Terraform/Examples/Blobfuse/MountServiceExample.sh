e69c2e6076cd8ad322eaa15642fde3167ae31ca2
Mikhail Rodionov <rodionovmv@gmail.com>
2022-07-13 07:40:30 +0200

    Update MountServiceExample.sh

--- Terraform/Examples/Blobfuse/MountServiceExample.sh
+++ Terraform/Examples/Blobfuse/MountServiceExample.sh
@@ -1,13 +1,23 @@
+# Install blob fuse
+sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
+yum install blobfuse -y
+# Create directories
+chown -R root:logreaders /mnt/data/blobfusetmp
+chown -R root:logreaders /mnt/data/logs
+chmod -R 770 /mnt/data/blobfusetmp
+chmod -R 770 /mnt/data/logs
+# Service to mount/unmount container
+# -allow_other flag is used to ensure that not only owner will be able to access mounted files (otherwise root only will have access)
 cat << EOF > /opt/blobfusemount-start.sh
 #!/bin/bash
 mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && sudo umount /mnt/data/logs
-blobfuse /mnt/data/logs --tmp-path=/mnt/resource/blobfusetmp  --config-file=/etc/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
+blobfuse /mnt/logs --tmp-path=/mnt/data/blobfusetmp  --config-file=/etc/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -allow_other
 EOF
 chmod u+x /opt/blobfusemount-start.sh
 
 cat << EOF > /opt/blobfusemount-stop.sh
 #!/bin/bash
-mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && sudo umount /mnt/data/logs
+mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && fusermount /mnt/logs
 EOF
 chmod u+x /opt/blobfusemount-stop.sh
 
