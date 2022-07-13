3d65fa996ca47e5ff9ffb5276be847ac3f610b04
Mikhail Rodionov <rodionovmv@gmail.com>
2022-07-13 07:41:15 +0200

    Update MountServiceExample.sh

--- Terraform/Examples/Blobfuse/MountServiceExample.sh
+++ Terraform/Examples/Blobfuse/MountServiceExample.sh
@@ -10,14 +10,14 @@ chmod -R 770 /mnt/data/logs
 # -allow_other flag is used to ensure that not only owner will be able to access mounted files (otherwise root only will have access)
 cat << EOF > /opt/blobfusemount-start.sh
 #!/bin/bash
-mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && sudo umount /mnt/data/logs
+mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && fusermount -u /mnt/logs /mnt/data/logs
 blobfuse /mnt/logs --tmp-path=/mnt/data/blobfusetmp  --config-file=/etc/fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 -allow_other
 EOF
 chmod u+x /opt/blobfusemount-start.sh
 
 cat << EOF > /opt/blobfusemount-stop.sh
 #!/bin/bash
-mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && fusermount /mnt/logs
+mount | awk '{if (\$3 == "/mnt/data/logs") { exit 0}} ENDFILE{exit -1}' && fusermount -u /mnt/logs
 EOF
 chmod u+x /opt/blobfusemount-stop.sh
 
