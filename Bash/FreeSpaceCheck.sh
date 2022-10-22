7246adb9f10e4b48533d8b849eb8f0f48d94face
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2022-10-22 16:12:08 +0200

    Create FreeSpaceCheck.sh

---
+++ Bash/FreeSpaceCheck.sh
@@ -0,0 +1,7 @@
+# Condition to check for required space in target directory
+required_space=500
+required_space_kb=$(($required_space*1024*1024))
+free_space=$(df /dumps/DB-Dumps -k --output=avail | tail -1)
+if [[ $free_space -lt $required_space_kb ]]; then
+	echo "You have less than $required_space GB of free space in dumps folder, free up disk space and re-run this script"
+fi;
