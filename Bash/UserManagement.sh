aff3951a057c8d4430d6866a9339297db6dcbbf5
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2022-12-09 15:41:05 +0100

    Update UserManagement.sh

--- Bash/UserManagement.sh
+++ Bash/UserManagement.sh
@@ -2,18 +2,18 @@
 cut -d: -f1 /etc/passwd
 # List sudo users
 grep '^sudo:.*$' /etc/group | cut -d: -f4
+# View user's groups
+groups username
 # Create user & set password
 adduser username
 passwd username
+# Change user's display name
+sudo chfn -f "FirstName LastName" <username>
 # Create group
 groupadd groupname
 # Add user to group
 usermod -a -G examplegroup exampleusername
 # Add user to sudo group
 sudo usermod -aG sudo <username>
-# View user's groups
-groups username
-# Get existing users
-cut -d: -f1 /etc/passwd
 # Disable password expiration
 chage -I -1 -m 0 -M 99999 -E -1 username
