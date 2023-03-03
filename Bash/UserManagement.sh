d52b8207d6b05495c6c132c602ff7dfee0a97cf4
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2023-03-03 15:53:00 +0100

    Update UserManagement.sh

--- Bash/UserManagement.sh
+++ Bash/UserManagement.sh
@@ -1,19 +1,29 @@
-# List users
+## List users
 cut -d: -f1 /etc/passwd
-# List sudo users
+
+## List sudo users
 grep '^sudo:.*$' /etc/group | cut -d: -f4
-# View user's groups
+
+## View user's groups
 groups username
-# Create user & set password
+
+## Chec if user has sudo access
+sudo -l -U username # if output contains ALL : ALL for commands allowed to run that means sudo access
+# in case on not having sudo access output will be "User USERNAME is not allowed to run sudo on localhost."
+
+## Create user & set password
 adduser username
 passwd username
-# Change user's display name
+## Change user's display name
 sudo chfn -f "FirstName LastName" <username>
-# Create group
+
+## Create group
 groupadd groupname
-# Add user to group
+
+## Add user to group
 usermod -a -G examplegroup exampleusername
-# Add user to sudo group
+
+## Add user to sudo group
 sudo usermod -aG sudo <username>
-# Disable password expiration
+## Disable password expiration
 chage -I -1 -m 0 -M 99999 -E -1 username
